370c3a1bba891576816af33c03042b9e9cc62c66
sides1 <dima.litvinenko1990@gmail.com>
2023-03-27 00:12:35 +0300

    create api request

--- src/index.js
+++ src/index.js
@@ -1,3 +1,101 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import axios from 'axios';
 
-const DEBOUNCE_DELAY = 300;
+const API_KEY = '34757996-44fb61a2c5d4adca97d86e8d6';
+
+const searchForm = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+
+let searchQuery = '';
+let page = 1;
+let totalHits = 0;
+
+searchForm.addEventListener('submit', handleFormSubmit);
+loadMoreBtn.addEventListener('click', handleLoadMoreBtnClick);
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  gallery.innerHTML = '';
+  page = 1;
+  searchQuery = event.currentTarget.elements.searchQuery.value.trim();
+
+  if (!searchQuery) {
+    return Notiflix.Notify.warning('Enter a search term!');
+  }
+
+  fetchImages();
+}
+
+function handleLoadMoreBtnClick() {
+  page += 1;
+  fetchImages();
+  loadMoreBtn.classList.remove('.is-hidden');
+}
+
+function fetchImages() {
+  axios
+    .get(
+      `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
+    )
+    .then(({ data }) => {
+      const hits = data.hits;
+      totalHits = data.totalHits;
+
+      if (hits.length === 0) {
+        Notiflix.Notify.warning(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+        return;
+      }
+
+      const imagesMarkup = createImagesMarkup(hits);
+      gallery.insertAdjacentHTML('beforeend', imagesMarkup);
+
+      if (hits.length < 40) {
+        Notiflix.Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+        loadMoreBtn.style.display = 'none';
+      } else {
+        loadMoreBtn.style.display = 'block';
+        page += 1;
+      }
+    })
+    .catch(error => console.log(error));
+}
+
+function createImagesMarkup(images) {
+  return images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        likes,
+        views,
+        comments,
+        downloads,
+        tags,
+      }) => `
+        <div class="photo-card">
+          <img src="${webformatURL}" data-source="${largeImageURL}" alt="${tags}" loading="lazy" />
+          <div class="info">
+            <p class="info-item">
+              <b>Likes:</b> ${likes}
+            </p>
+            <p class="info-item">
+              <b>Views:</b> ${views}
+            </p>
+            <p class="info-item">
+              <b>Comments:</b> ${comments}
+            </p>
+            <p class="info-item">
+              <b>Downloads:</b> ${downloads}
+            </p>
+          </div>
+        </div>
+      `
+    )
+    .join('');
+}
